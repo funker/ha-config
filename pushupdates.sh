eac1d2001707998739bc2bd0733cb758c2f5c786
root <root@homeassistant.local.hass.io>
2026-02-08 14:59:39 +0100

    Config update: 2026-02-08 14:59:39

--- pushupdates.sh
+++ pushupdates.sh
@@ -1,4 +1,6 @@
-cd /config
+#!/bin/bash
+# pushupdates.sh
+cd /config || exit 1
 git add .
-git commit -m "Config files on `date + '%Y-%m-%d %H:%M:%S'`"
-git push -u origin master
+git commit -m "Config update: $(date +'%Y-%m-%d %H:%M:%S')" || true
+git push origin master
